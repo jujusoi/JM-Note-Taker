cfddb794e5645493f2ad6ac06f38db6fb3fc2b7c
jujusoi <jalxmcdonald@hotmail.com>
2023-08-22 11:28:54 +0930

    added 404 page

--- Develop/server.js
+++ Develop/server.js
@@ -15,6 +15,10 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/404.html'));
+})
+
 app.get('/api/notes', (req, res) => {
     res.json(database);
 });
