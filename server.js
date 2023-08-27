e5a39cfbd1eda07d945a21406aa7cf67c7d1593a
jujusoi <jalxmcdonald@hotmail.com>
2023-08-27 18:03:10 +0930

    moved server up dir

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const index = require('./public/routes/index.js');
+const index = require('./Develop/public/routes/index.js');
 const app = express();
 const port = process.env.PORT || 4023;
 
